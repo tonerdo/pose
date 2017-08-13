a917dcd84772f53064766fa533f8afc532c6eb09
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-08-13 12:40:19 +0100

    add IsolationContext class

---
+++ src/Focks/IsolationContext.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Focks
+{
+    public class IsolationContext
+    {
+        public IsolationContext(Action entry, params Shim[] shims)
+        {
+            
+        }
+    }
+}
