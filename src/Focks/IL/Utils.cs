ac10cb0ab7010fcab6bb402f7c53ed1a32097c14
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-08-20 01:18:06 +0100

    add IL related utility methods

---
+++ src/Focks/IL/Utils.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Focks.IL
+{
+    internal class Utils
+    {
+        public static MethodInfo FindMethod(Type type, int metadataToken, string genericArguments)
+        {
+            MethodInfo method = type.Module.ResolveMember(metadataToken) as MethodInfo;
+            Type[] typeArguments = ParseGenericArguments(genericArguments);
+            if (typeArguments.Length > 0)
+                method = method.MakeGenericMethod(typeArguments);
+
+            return method;
+        }
+
+        public static IntPtr GetMethodPointer(MethodInfo methodInfo)
+        {
+            if (methodInfo is DynamicMethod)
+            {
+                var methodDescriptior = typeof(DynamicMethod).GetMethod("GetMethodDescriptor", BindingFlags.Instance | BindingFlags.NonPublic);
+                return ((RuntimeMethodHandle)methodDescriptior.Invoke(methodInfo as DynamicMethod, null)).GetFunctionPointer();
+            }
+
+            return methodInfo.MethodHandle.GetFunctionPointer();
+        }
+
+        private static Type[] ParseGenericArguments(string genericArguments)
+        {
+            Type[] typeArguments = new Type[0];
+            int index = genericArguments.IndexOf("[");
+            if (index == -1)
+                return typeArguments;
+            
+            string generics = genericArguments.Substring(index);
+            genericArguments = genericArguments.Substring(0, index);
+            generics = generics.Replace("[", "").Replace("]", "");
+
+            string[] split = generics.Split(','); 
+            typeArguments = split.Select(s => FindType(IsolationContext.EntryPointType.Assembly, s)).ToArray();
+            return typeArguments;
+        }
+
+        public static Type FindType(Assembly assembly, string type)
+        {
+            Type t = assembly.GetType(type);
+            if (t != null)
+                return t;
+            
+            foreach (var asm in assembly.GetReferencedAssemblies())
+            {
+                t = FindType(Assembly.Load(asm), type);
+                if (t != null)
+                    return t;
+            }
+
+            return null;
+        }
+    }
+}
