5605063de6fea34fe4987230b48a8e0a395720aa
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-08-20 01:19:39 +0100

    update IsolationContext constructor to use new IsolationMode

--- src/Focks/IsolationContext.cs
+++ src/Focks/IsolationContext.cs
@@ -2,37 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using Focks.DependencyAnalysis;
-using Focks.Extensions;
 using Focks.IL;
 
 namespace Focks
 {
     public class IsolationContext
     {
-        public IsolationContext(Action entry, params Shim[] shims)
+        internal static Type EntryPointType;
+
+        public IsolationContext(Action entryPoint, params Shim[] shims)
         {
-            CallAnalyzer analyzer = CallAnalyzer.CreateAnalyzer(entry.Method, shims);
-            CallGraph callGraph = analyzer.GenerateCallGraph();
-
-            List<CallNode> shimNodes = new List<CallNode>();
-            foreach (var shim in shims)
-            {
-                CallNode node = callGraph.FirstOrDefault(g => g.Name == shim.Original.ToFullString());
-                if (node != null)
-                    shimNodes.Add(node);
-            }
-
-            CallGraph subGraph = analyzer.GenerateCallGraph(shimNodes);
-            CallGraphRewriter rewriter = CallGraphRewriter.CreateRewriter(subGraph, shims);
-            var entrypoint = rewriter.Rewrite();
-
-            object[] args = new object[shims.Length + 1];
-            args[0] = entry.Target;
-            for (int i = 0; i < shims.Length; i++)
-                args[i + 1] = shims[i].Replacement.Target;
-
-            entrypoint.Invoke(null, args);
+            EntryPointType = entryPoint.Target.GetType();
+            Type delegateType = typeof(Action<>).MakeGenericType(entryPoint.Target.GetType());
+            MethodRewriter rewriter = MethodRewriter.CreateRewriter(entryPoint.Method);
+            rewriter.Rewrite().CreateDelegate(delegateType).DynamicInvoke(entryPoint.Target);
         }
     }
 }
