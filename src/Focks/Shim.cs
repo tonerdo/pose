c861304809fc90ca58eac3028001c49825720bee
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-08-20 14:25:48 +0100

    update Shim public api

--- src/Focks/Shim.cs
+++ src/Focks/Shim.cs
@@ -43,7 +43,7 @@ namespace Focks
             return new Shim(GetMethodFromExpression(original.Body), null);
         }
 
-        public Shim With(Delegate replacement)
+        private Shim WithImpl(Delegate replacement)
         {
             if (!SignatureEquals(_original, replacement.Method))
                 throw new Exception("Signature mismatch");
@@ -51,6 +51,64 @@ namespace Focks
             _replacement = replacement;
             return this;
         }
+
+        public Shim With(Action replacement) => WithImpl(replacement);
+
+        public Shim With<T>(Action<T> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2>(Action<T1, T2> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3>(Action<T1, T2, T3> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4>(Action<T1, T2, T3, T4> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> replacement)
+            => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<TResult>(Func<TResult> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, TResult>(Func<T1, TResult> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2, TResult>(Func<T1, T2, TResult> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> replacement) => WithImpl(replacement);
+
+        public Shim With<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> replacement)
+            => WithImpl(replacement);
+        
+        public Shim With<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> replacement)
+            => WithImpl(replacement);
 
         private static MethodBase GetMethodFromExpression(Expression expression)
         {
