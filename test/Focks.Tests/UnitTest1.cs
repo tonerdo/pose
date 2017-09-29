0834498298367d3beca026165fd59ea1a7d85dbe
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-09-29 18:14:49 +0100

    add test project

---
+++ test/Focks.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Focks.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
