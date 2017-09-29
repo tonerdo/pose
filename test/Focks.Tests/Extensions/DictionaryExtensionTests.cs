99298b5104a8c38f7a6472f4016b9e5ab9e5f31e
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-09-29 18:21:34 +0100

    add DictionaryExtension Tests

---
+++ test/Focks.Tests/Extensions/DictionaryExtensionTests.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+using Focks.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Focks.Tests
+{
+    [TestClass]
+    public class DictionaryExtensionTests
+    {
+        [TestMethod]
+        public void TestTryAdd()
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            Assert.AreEqual<bool>(true, dictionary.TryAdd(0, "0"));
+            Assert.AreEqual<bool>(false, dictionary.TryAdd(0, "1"));
+        }
+    }
+}
