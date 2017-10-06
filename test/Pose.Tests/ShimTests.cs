da5620a114a08bd0d5056b92e4adf24fd1fccf75
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-10-06 12:07:10 +0100

    add tests for Shim class

---
+++ test/Pose.Tests/ShimTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Reflection;
+
+using Pose.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static System.Console;
+
+namespace Pose.Tests
+{
+    [TestClass]
+    public class ShimTests
+    {
+        [TestMethod]
+        public void TestReplace()
+        {
+            Shim shim = Shim.Replace(() => Console.WriteLine(""));
+
+            Assert.AreEqual(typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }), shim.Original);
+            Assert.IsNull(shim.Replacement);
+        }
+
+        [TestMethod]
+        public void TestReplaceWithInstanceVariable()
+        {
+            ShimTests shimTests = new ShimTests();
+            Shim shim = Shim.Replace(() => shimTests.TestReplace());
+
+            Assert.AreEqual(typeof(ShimTests).GetMethod("TestReplace"), shim.Original);
+            Assert.AreSame(shimTests, shim.Instance);
+            Assert.IsNull(shim.Replacement);
+        }
+
+        [TestMethod]
+        public void TestShimReplaceWithInvalidSignature()
+        {
+            ShimTests shimTests = new ShimTests();
+            Shim shim = Shim.Replace(() => shimTests.TestReplace());
+            Assert.ThrowsException<Exception>(
+                () => Shim.Replace(() => shimTests.TestReplace()).With(() => { }));
+            Assert.ThrowsException<Exception>(
+                () => Shim.Replace(() => Console.WriteLine(Of.Type<string>())).With(() => { }));
+        }
+
+        [TestMethod]
+        public void TestShimReplaceWith()
+        {
+            ShimTests shimTests = new ShimTests();
+            Action action = new Action(() => { });
+            Action<ShimTests> actionInstance = new Action<ShimTests>((s) => { });
+
+            Shim shim = Shim.Replace(() => Console.WriteLine()).With(action);
+            Shim shim1 = Shim.Replace(() => shimTests.TestReplace()).With(actionInstance);
+
+            Assert.AreEqual(typeof(Console).GetMethod("WriteLine", Type.EmptyTypes), shim.Original);
+            Assert.AreEqual(action, shim.Replacement);
+
+            Assert.AreEqual(typeof(ShimTests).GetMethod("TestReplace"), shim1.Original);
+            Assert.AreSame(shimTests, shim1.Instance);
+            Assert.AreEqual(actionInstance, shim1.Replacement);
+        }
+    }
+}
